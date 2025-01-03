92d989f569674da51a555f60b42fdab85a9fa93b
rocketcamel <87954986+rocketcamel@users.noreply.github.com>
2025-01-02 20:07:46 -0800

    Transparent background + telescope conf (#1)

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,41 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "tokyonight",
+  hl_override = {
+    Normal = {
+      bg = "NONE",
+    },
+    Float = {
+      bg = "NONE",
+    },
+    NvimTreeNormal = {
+      bg = "NONE",
+    },
+    NvimTreeNormalNC = {
+      bg = "NONE",
+    },
+    TbFill = {
+      bg = "NONE",
+    },
+    TbBufOn = {
+      bg = "NONE",
+    },
+    TbBufOff = {
+      bg = "NONE",
+    },
+    TbBufOnClose = { bg = "NONE" },
+    TbBufOffClose = { bg = "NONE" },
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
