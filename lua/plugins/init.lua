5d99a6ea521f66bb47db08ee04719c65cbafd7d2
lucwise-13257 <luca_lise@icloud.com>
2025-01-02 20:07:07 -0800

    Transparent background + telescope conf

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -43,4 +43,15 @@ return {
       },
     },
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+          ".git",
+        },
+      },
+    },
+  },
 }
